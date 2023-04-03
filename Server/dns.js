cd9b7d0760f6431ea6adcf58f15d8ade747a804e
Ketho <sghs17@outlook.com>
2023-04-03 14:11:37 +0200

    add AAAA records to dns

--- Server/dns.js
+++ Server/dns.js
@@ -3,60 +3,71 @@ const dns2 = require('dns2');
 
 const { Packet } = dns2;
 
+const data = {
+	// devices
+	'hq-r1.pjtir6.net'   : '2a02:a420:b:110::f:0',
+	'hq-r2.pjtir6.net'   : '2a02:a420:b:111::f:0',
+	'hq-dls1.pjtir6.net' : '2a02:a420:b:120::f:0',
+	'hq-dls2.pjtir6.net' : '2a02:a420:b:121::f:0',
+	'hq-als1.pjtir6.net' : '2a02:a420:b:1a0::2',
+	'hq-als2.pjtir6.net' : '2a02:a420:b:1a0::3',
+
+	'isp.pjtir6.net'     : '2a02:a420:b:300::f:0',
+
+	'br-r1.pjtir6.net'   : '2a02:a420:b:210::f:0',
+	'br-dls1.pjtir6.net' : '2a02:a420:b:220::f:0',
+	'br-als1.pjtir6.net' : '2a02:a420:b:2a0::2',
+
+	// hosts
+	'hq-pc1.pjtir6.net'  : '2a02:a420:b:1a1::10',
+	'dns.pjtir6.net'     : '2a02:a420:b:1a2::10',
+	'hq-pc2.pjtir6.net'  : '2a02:a420:b:1a3::10',
+
+	'br-pc1.pjtir6.net'  : '2a02:a420:b:2a1::10',
+	'br-pc2.pjtir6.net'  : '2a02:a420:b:2a2::10',
+};
+
 const server = dns2.createServer({
-  udp: true,
-  handle: (request, send, rinfo) => {
-    const response = Packet.createResponseFromRequest(request);
-    const [question] = request.questions;
-    if (request.questions[0].name == "dns.pjtir6.net") {
-      console.log("# DNS")
-      const { name } = question;
-      response.answers.push({
-        name,
-        // type: Packet.TYPE.A, // IPv4
-        type: Packet.TYPE.AAAA, // IPv6
-        class: Packet.CLASS.IN,
-        ttl: 300,
-        address: '2a02:a420:b:1a2::10'
-      });
-      send(response);
-    }
-    else if (request.questions[0].name == "hq-pc1") {
-      console.log("# HQ-PC1")
-      const { name } = question;
-      response.answers.push({
-        name,
-        type: Packet.TYPE.AAAA,
-        class: Packet.CLASS.IN,
-        ttl: 300,
-        address: '2a02:a420:b:1a2::10'
-      });
-      send(response);
-    }
-  }
+	udp: true,
+	handle: (request, send, rinfo) => {
+		const response = Packet.createResponseFromRequest(request);
+		const [question] = request.questions;
+		if (data[request.questions[0].name]) {
+			console.log("# ", request.questions[0].name)
+			const { name } = question;
+			response.answers.push({
+				name,
+				type: Packet.TYPE.AAAA, // IPv6
+				class: Packet.CLASS.IN,
+				ttl: 300,
+				address: data[request.questions[0].name]
+			});
+			send(response);
+		}
+	}
 });
 
 server.on('request', (request, response, rinfo) => {
-  console.log('request', request.header.id, request.questions[0]);
+	console.log('request', request.header.id, request.questions[0]);
 });
 
 server.on('requestError', (error) => {
-  console.log('Client sent an invalid request', error);
+	console.log('Client sent an invalid request', error);
 });
 
 server.on('listening', () => {
-  console.log('listening', server.addresses());
+	console.log('listening', server.addresses());
 });
 
 server.on('close', () => {
-  console.log('server closed');
+	console.log('server closed');
 });
 
 server.listen({
-  udp: {
-    port: 53,
-    address: "2a02:a420:b:1a2::10",
-  },
+	udp: {
+		port: 53,
+		address: "2a02:a420:b:1a2::10",
+	},
 });
 
 // server.close();
